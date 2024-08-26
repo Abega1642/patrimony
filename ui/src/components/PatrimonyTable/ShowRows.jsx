115656175ef4ffda5e89a86640b055f70c54e0f8
Abega1642 <hei.abega.2@gmail.com>
2024-08-26 23:24:19 +0300

    chore: transform ShowRows into React component

--- ui/src/components/PatrimonyTable/ShowRows.jsx
+++ ui/src/components/PatrimonyTable/ShowRows.jsx
@@ -1,17 +1,35 @@
-export default function ShowRows(data) {
+export default function ShowRows({possesseur, possessions}) {
     return (
-                data.possessions.map(pos => {
-                    return (
-                        <tr key={pos.libelle}>
-                            <td>{data.possesseur.possesseur.nom}</td>
-                            <td>{pos.libelle}</td>
-                            <td>{pos.valeur}</td>
-                            <td>{pos.dateDebut.toLocaleDateString()}</td>
-                            <td>{pos.dateFin.toLocaleDateString()}</td>
-                            <td>{pos.tauxAmortissement}</td>
-                            <td>{Math.abs(pos.getValeur(new Date()))}</td>
-                        </tr>
-                    )
-                })      
+        <>
+            <thead>
+                <tr>
+                    <th>Possesseur</th>
+                    <th>Libellé</th>
+                    <th>Valeur</th>
+                    <th>Date de début</th>
+                    <th>Date de fin</th>
+                    <th>Taux d`Amortissement</th>
+                    <th>Valeur actuel</th>
+                </tr>
+            </thead>
+            <tbody>
+                {console.log(possesseur)}
+                {
+                    possessions.map(pos => {
+                        return (
+                            <tr key={pos.libelle}>
+                                <td>{possesseur}</td>
+                                <td>{pos.libelle}</td>
+                                <td>{pos.valeur}</td>
+                                <td>{pos.dateDebut.toLocaleDateString()}</td>
+                                <td>{pos.dateFin.toLocaleDateString()}</td>
+                                <td>{pos.tauxAmortissement}</td>
+                                <td>{Math.abs(pos.getValeur(new Date()))}</td>
+                            </tr>
+                        )
+                    }) 
+                }     
+            </tbody>
+        </>
     )
 }
