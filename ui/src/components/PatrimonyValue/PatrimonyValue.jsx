7c6fe61623182e06c93d11e079dd65be183b5790
Abega1642 <hei.abega.2@gmail.com>
2024-08-26 20:27:51 +0300

    feat: add ui/src/components/UnchangedComponents/function/fetchData.js to separate fetchData function

--- ui/src/components/PatrimonyValue/PatrimonyValue.jsx
+++ ui/src/components/PatrimonyValue/PatrimonyValue.jsx
@@ -1,57 +1,13 @@
 import { useState, useEffect } from "react";
-import Flux from "../../../../models/possessions/Flux.js";
-import Possession from "../../../../models/possessions/Possession.js";
 import Patrimoine from "../../../../models/Patrimoine.js";
+import { fetchData } from "../UnchangedComponents/function/fetchData.js";
 
 function PatrimonyValue() {
     const [patrimony, setPatrimony] = useState(new Patrimoine("", []));
     const [evaluationDate, setEvaluationDate] = useState(new Date());
 
     useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await fetch('http://localhost:3001/possession');
-                const result = await response.json();
-
-                if (result.status === 'OK') {
-                    const possessions = result.data.data[1].data.possessions.map(possessionData => {
-                        const numParams = Object.keys(possessionData).length;
-                        const isFlux = (numParams === 8);
-                        if (isFlux) {
-                            
-                            const anw = new Flux(
-                                possessionData.possesseur,
-                                possessionData.libelle,
-                                possessionData.valeurConstante,
-                                new Date(possessionData.dateDebut),
-                                new Date(possessionData.dateFin),
-                                possessionData.tauxAmortissement,
-                                possessionData.jour
-                            );
-                            return anw
-                            
-                        } else {
-                            return new Possession(
-                                possessionData.possesseur,
-                                possessionData.libelle,
-                                possessionData.valeur,
-                                new Date(possessionData.dateDebut),
-                                new Date(possessionData.dateFin),
-                                possessionData.tauxAmortissement
-                            );
-                        }
-                    });
-                    setPatrimony(new Patrimoine(result.data.data[1].data, possessions))
-                    
-                } else {
-                    console.error('Erreur lors de la lecture des données');
-                }
-            } catch (error) {
-                console.error('Erreur lors de la requête :', error);
-            }
-        }
-
-        fetchData();
+        fetchData(setPatrimony);
     }, []);
 
     const handleDateChange = (e) => {
---
+++ ui/src/components/UnchangedComponents/function/fetchData.js
@@ -0,0 +1,46 @@
+import Flux from "../../../../../models/possessions/Flux";
+import Possession from "../../../../../models/possessions/Possession";
+import Patrimoine from "../../../../../models/Patrimoine";
+
+export async function fetchData(setPatrimony) {
+    try {
+        const response = await fetch('http://localhost:3001/possession');
+        const result = await response.json();
+
+        if (result.status === 'OK') {
+            const possessions = result.data.data[1].data.possessions.map(possessionData => {
+                const numParams = Object.keys(possessionData).length;
+                const isFlux = (numParams === 8);
+                if (isFlux) {
+                    
+                    const anw = new Flux(
+                        possessionData.possesseur,
+                        possessionData.libelle,
+                        possessionData.valeurConstante,
+                        new Date(possessionData.dateDebut),
+                        new Date(possessionData.dateFin),
+                        possessionData.tauxAmortissement,
+                        possessionData.jour
+                    );
+                    return anw
+                    
+                } else {
+                    return new Possession(
+                        possessionData.possesseur,
+                        possessionData.libelle,
+                        possessionData.valeur,
+                        new Date(possessionData.dateDebut),
+                        new Date(possessionData.dateFin),
+                        possessionData.tauxAmortissement
+                    );
+                }
+            });
+            setPatrimony(new Patrimoine(result.data.data[1].data, possessions))
+            
+        } else {
+            console.error('Erreur lors de la lecture des données');
+        }
+    } catch (error) {
+        console.error('Erreur lors de la requête :', error);
+    }
+}
