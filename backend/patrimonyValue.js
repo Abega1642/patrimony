3b5c52552d2163c588a4b54552125086bba2e80c
Abega1642 <hei.abega.2@gmail.com>
2024-08-27 19:14:35 +0300

    chore: add two kind of getPatrimonyValue in patrimonyValue.js

--- backend/patrimonyValue.js
+++ backend/patrimonyValue.js
@@ -1,7 +1,13 @@
+import Possession from '../models/possessions/Possession.js'
+import Flux from '../models/possessions/Flux.js'
+import Patrimoine from '../models/Patrimoine.js'
+import getAllPossessions from './getAllPossessions.js';
 
-async function getPatrimonyValue() {
-    const datas = await read('../data/data.json').json();
-    const possessions = datas.data.data[1].data.possessions.map(possessionData => {
+export async function getPatrimonyValueNow() {
+
+    const possessionsData = await getAllPossessions();
+    
+    const possessions = possessionsData.map(possessionData => {
         const numParams = Object.keys(possessionData).length;
         const isFlux = (numParams === 8);
         if (isFlux) {
@@ -28,5 +34,44 @@ async function getPatrimonyValue() {
             );
         }
     });
+    const Patrimony = new Patrimoine("", possessions)
+    return Patrimony.getValeur(new Date())
 }
 
+export async function getPatrimonyValueByDate(date) {
+
+    const possessionsData = await getAllPossessions();
+    
+    const possessions = possessionsData.map(possessionData => {
+        const numParams = Object.keys(possessionData).length;
+        const isFlux = (numParams === 8);
+        if (isFlux) {
+            
+            const anw = new Flux(
+                possessionData.possesseur,
+                possessionData.libelle,
+                possessionData.valeurConstante,
+                new Date(possessionData.dateDebut),
+                new Date(possessionData.dateFin),
+                possessionData.tauxAmortissement,
+                possessionData.jour
+            );
+            return anw
+            
+        } else {
+            return new Possession(
+                possessionData.possesseur,
+                possessionData.libelle,
+                possessionData.valeur,
+                new Date(possessionData.dateDebut),
+                new Date(possessionData.dateFin),
+                possessionData.tauxAmortissement
+            );
+        }
+    });
+
+    const Patrimony = new Patrimoine("", possessions);
+
+    return Patrimony.getValeur(date);
+
+}
