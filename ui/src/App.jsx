bf4418da106427fac0bb2336f0eb4c30a68cc599
Abega1642 <hei.abega.2@gmail.com>
2024-08-28 19:31:27 +0300

    feat: add routes to pages

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -4,18 +4,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { PossessionsList } from "./components/PossessionsList/PatrimonyTable.jsx";
 import './App.css'
 import PatrimonyValue from "./components/PatrimonyValue/PatrimonyValue.jsx";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 function App() {
 
   return (
     <>
-      <NavBar/>
-      <main className="main">
-        <PossessionsList />
-        <PatrimonyValue />
-      </main>
-      <Footer/>
+      <BrowserRouter>
+        <NavBar />
+          <Routes>
+            <Route path='/possession' element = {
+              <main className="main">
+                <PossessionsList />
+                <PatrimonyValue />
+              </main>
+              } 
+            />
+            <Route path='/patrimoine' element = {null} />
+            <Route path='/possession/edit' element = {null} />
+          </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
