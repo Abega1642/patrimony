660036f85029018925fc2e577bfb60b480a628a5
Abega1642 <hei.abega.2@gmail.com>
2024-08-28 18:01:01 +0300

    feat: add getPossesionsValues.js file in /functions/possessions in backend side

---
+++ backend/functions/possessions/getPossessionsValues.js
@@ -0,0 +1,44 @@
+import Possession from "../../../models/possessions/Possession.js";
+import Flux from "../../../models/possessions/Flux.js";
+import getAllPossessions from "./getAllPossessions.js";
+
+export default async function getPossessionsWithValues() {
+    const possessions = await getAllPossessions()
+
+    const possessionsValues = possessions.map(possessionData => {
+        const numParams = Object.keys(possessionData).length;
+        const isFlux = (numParams === 8);
+        if (isFlux) {
+            
+            const anw = new Flux(
+                possessionData.possesseur,
+                possessionData.libelle,
+                possessionData.valeurConstante,
+                new Date(possessionData.dateDebut),
+                new Date(possessionData.dateFin),
+                possessionData.tauxAmortissement,
+                possessionData.jour
+            );
+            return anw
+            
+        } else {
+            return new Possession(
+                possessionData.possesseur,
+                possessionData.libelle,
+                possessionData.valeur,
+                new Date(possessionData.dateDebut),
+                new Date(possessionData.dateFin),
+                possessionData.tauxAmortissement
+            );
+        }
+    }).map(possession => {
+        return {
+           "actualValue" : possession.getValeur(new Date())
+        }
+    });
+
+    return possessions.map((possession, ind) => {
+        return {... possession, ... possessionsValues[ind]}
+        }
+    ) 
+}
