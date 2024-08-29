c06da0c19d7d4ce60490d4acb758ee5c6e52f2ed
Abega1642 <hei.abega.2@gmail.com>
2024-08-29 20:18:54 +0300

    feat: add flex property to section in linechart component

--- ui/src/components/Chart/LineChart.jsx
+++ ui/src/components/Chart/LineChart.jsx
@@ -73,7 +73,7 @@ function LineChart() {
     };
 
     return (
-        <section className=''>
+        <section className='d-flex flex-wrap'>
             <div className='card col-md-6 offset-md-3 mt-5 p-4 shadow-lg'>
                 <h3 className='text-center mb-4'>Graphique en ligne des valeurs de patrimoine</h3>
                 <div className='chart-container bg-light p-3 rounded'>
